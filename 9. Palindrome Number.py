fb44c81f74769e699810c8e1d6bbd1790901f6ca
Ivan Tochilkin <ivan.tochilkin@smiledirectclub.com>
2022-03-16 02:10:52 +0300

    autoformat

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -1,7 +1,7 @@
 def isPalindrome(x: int) -> bool:
     s = str(x)
     for ind in range(0, len(s) // 2):
-        if s[ind] != s[len(s) - 1 - ind]:
+        if s[ind] != s[len(s) - 1 - ind]:  # s[-1 - ind]
             return False
 
     return True
