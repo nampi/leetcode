b417acdae83684fec76c27dc688fa2ea1396268a
Ivan Tochilkin <ivan.tochilkin@smiledirectclub.com>
2022-03-16 00:49:20 +0300

    autoformat

--- 1221. Split a String in Balanced Strings.py
+++ 1221. Split a String in Balanced Strings.py
@@ -6,9 +6,9 @@ def balancedStringSplit(self, s: str) -> int:  # это был метод кла
             cursum += 1
         else:
             cursum -= 1
-        if cursum == 0:
+        if cursum == 0:  # можно писать if not cursum, но так тоже вполне можно
             result += 1
-            cursum = 0
+            cursum = 0  # она уже равна нулю
     return result
 
 
