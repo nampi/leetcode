8866e226df4ac5e7b28339df4957be817ecb7e8c
Trish <namp1@yandex.ru>
2022-03-16 01:23:52 +0300

    old work

---
+++ 28. Implement strStr.py
@@ -0,0 +1,12 @@
+def strStr(self, haystack: str, needle: str) -> int:
+    if needle == "":
+        return 0
+
+    for i in range(len(haystack) + 1 - len(needle)):
+        if haystack[i:i + len(needle)] == needle:
+            return i
+
+    return -1
+
+O(n * m)
+O(1)
