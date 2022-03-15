52d704dbb3f7ec9f45752d8b8f48a8feb65af456
Ivan Tochilkin <ivan.tochilkin@smiledirectclub.com>
2022-03-16 01:43:23 +0300

    autoformat

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,5 +1,5 @@
-def twoSum(nums: [], target: int) -> []:  # why do we need List[int]
-    dict = {}
+def twoSum(nums: [], target: int) -> []:  # лучше писать list
+    dict = {} # встроенное имя, нужно заменить.
     for index, x in enumerate(nums):
         if x in dict:
             return dict[x], index  # do we need to return [dict[x], index]
